8ed71e3ccad4c904fc66514b67ae5bbba5a151e6
JuhelM07 <37294796+JuhelM07@users.noreply.github.com>
2018-12-01 01:06:58 +0000

    Add files via upload

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from tutorial import views
 from django.conf.urls import url, include
 from tutorial import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -9,4 +11,4 @@ urlpatterns = [
     url(r'^$', views.login_redirect, name= 'login_redirect'),
     url('admin/', admin.site.urls),
     url(r'^account/', include('account.urls', namespace='account'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
