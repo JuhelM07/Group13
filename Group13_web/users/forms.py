9a8ba44559bd3db999472c43033eb39dd979debf
mtleukhan <30444728+mtleukhan@users.noreply.github.com>
2018-12-13 11:38:53 +0000

    Add files via upload

--- Group13_web/users/forms.py
+++ Group13_web/users/forms.py
@@ -3,6 +3,7 @@ import datetime
 from dobwidget import DateOfBirthWidget
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -11,3 +12,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'gender', 'birth_date']
