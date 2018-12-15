7ba2a1eda8f78e8decdaf4f7ed7e4088fd42bdf0
JuhelM07 <37294796+JuhelM07@users.noreply.github.com>
2018-12-15 15:38:42 +0000

    Add files via upload

---
+++ Group13_web/users/choices.py
@@ -0,0 +1,22 @@
+GENDER_CHOICES = (
+        ("Male", ("Male")),
+        ("Female", ("Female")),
+        ("Other", ("Other"))
+)
+
+
+
+HOBBY_CHOICES = (
+        ("Football", ("Football")),
+        ("Tennis", ("Tennis")),
+        ("Swimming", ("Swimming")),
+        ("Piano", ("Piano")),
+        ("Karate", ("Karate")),
+        ("Baking", ("Baking")),
+        ("Painting", ("Painting")),
+        ("Dancing", ("Dancing")),
+        ("Reading", ("Reading")),
+        ("Watching movies", ("Watching movies")),
+)
+
+
--- Group13_web/users/views.py
+++ Group13_web/users/views.py
@@ -8,8 +8,13 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            user.refresh_from_db()
             username = form.cleaned_data.get('username')
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.profile.gender= form.cleaned_data.get('gender')
+            user.profile.hobbies= form.cleaned_data.get('hobbies')
+            user.save()
             messages.success(request, f'Account created for {username}! You are now able to log in')
             return redirect('login')
     else:
@@ -19,3 +24,7 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+def match(request):
+    return render(request, 'users/matches.html')
+    profile = request.user.get_profile()
